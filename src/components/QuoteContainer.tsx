62e3b00ed830bcd1341d696628489bdc0bff11d5
yosefbeder <dryosefbeder@gmail.com>
2021-10-03 07:33:25 +0200

    Add a caret and animated it

--- src/components/QuoteContainer.tsx
+++ src/components/QuoteContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AnimateSharedLayout, motion, useCycle } from 'framer-motion';
 
 interface QuoteContainerProps {
   quote: string;
@@ -6,36 +7,60 @@ interface QuoteContainerProps {
   isTyping: boolean;
 }
 
+const variants: any = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
 const QuoteContainer: React.FC<QuoteContainerProps> = ({
   quote,
   typed,
   isTyping,
 }) => {
+  const [animation, cycle] = useCycle('visible', 'hidden');
+
   return (
-    <p className="text-gray-400 p-2 select-none">
-      <span className="text-gray-700 select-text">
-        {quote
-          .slice(0, typed.length)
-          .split('')
-          .map((l, i) =>
-            l === typed[i] ? (
-              l
-            ) : (
-              <span key={i} className="text-red-500 border-b-2 border-red-500">
-                {l}
-              </span>
-            ),
-          )}
-      </span>
-      <span
-        className={`text-blue-500 border-b-2 border-blue-500 ${
-          isTyping ? 'animate-none' : 'animate-pulse'
-        }`}
-      >
-        {quote[typed.length]}
-      </span>
-      {quote.slice(typed.length + 1)}
-    </p>
+    <AnimateSharedLayout>
+      <p className="text-gray-400 p-2 select-none">
+        <span className="text-gray-700 select-text">
+          {quote
+            .slice(0, typed.length)
+            .split('')
+            .map((l, i) =>
+              l === typed[i] ? (
+                l
+              ) : (
+                <span
+                  key={i}
+                  className="text-red-500 border-b-2 border-red-500"
+                >
+                  {l}
+                </span>
+              ),
+            )}
+        </span>
+        <span className="relative">
+          {quote[typed.length]}
+          <motion.div
+            layoutId="caret"
+            variants={variants}
+            initial="hidden"
+            transition={{
+              opacity: { type: 'spring' },
+              default: { type: 'tween', duration: 0.15, ease: 'easeOut' },
+            }}
+            animate={isTyping ? 'visible' : animation}
+            onAnimationComplete={() => cycle()}
+            className="absolute left-0 top-0 h-full w-0.5 bg-blue-400"
+          ></motion.div>
+        </span>
+        {quote.slice(typed.length + 1)}
+      </p>
+    </AnimateSharedLayout>
   );
 };
 
